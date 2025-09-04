4be094ba8af5c5425cece1fd39fd378de286bb13
Mart <39549352+SmartOnStuff@users.noreply.github.com>
2025-09-04 06:30:18 +0200

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,23 +1,19 @@
 import GetStarted from './GetStarted';
 import Features from './Features'; 
-import HyperHack from './HyperHack';
 import CardDeal from './CardDeal';
 import Clients from './Clients';
 import Footer from './Footer';
 import Hero from './Hero';
 import Navbar from './Navbar';
 import Stats from './Stats';
-import CampaignPopup from './CampaignPopup';
 
 export {
     GetStarted,
     Features, 
-    HyperHack,
     CardDeal,
     Clients,
     Footer,
     Hero,
     Navbar,
-    Stats,
-    CampaignPopup
+    Stats
 }
